37b2072763dce34842fff32ad0cef9c57f3a43fb
Clemens Fehr <clemensfehr@gmx.de>
2018-06-10 13:20:26 +0200

    byte string conversion

--- php/src/curve25519.php
+++ php/src/curve25519.php
@@ -1,33 +1,55 @@
 <?php
-declare(strict_types=1);
+declare(strict_types = 1);
 
 final class Curve25519
 {
 
-		public static $p;
-		public static $p_minus2;
-		public static $a24;
+    public static $p;
+
+    public static $p_minus2;
+
+    public static $a24;
 
     public function __construct()
     {
-        //$this->ensureIsValidEmail($email);
-				// calculate p as 2 ^ 255 - 19
-				self::$p = "2";
-				self::$p = bcpow(self::$p, "255");
-				self::$p = bcsub(self::$p, "19");
-				self::$p_minus2 = bcsub(self::$p, "2");
-				self::$a24 = "121665";
-
+        // $this->ensureIsValidEmail($email);
+        // calculate p as 2 ^ 255 - 19
+        self::$p = "2";
+        self::$p = bcpow(self::$p, "255");
+        self::$p = bcsub(self::$p, "19");
+        self::$p_minus2 = bcsub(self::$p, "2");
+        self::$a24 = "121665";
     }
 
     public function __toString(): string
     {
-        return "p=".self::$p;
+        return "p=" . self::$p;
     }
-
+    
+    /**
+     * Convert Hex String FA01EE... to int array 0xfa,0x01,0xee
+     * Hex byte arrays are strings in PHP.
+     * @return string
+     */
+    public function toByteArray(string $hexString): array
+    {
+        // split hex string up to bytes (2 hex digits -> one byte)
+        $a = str_split(pack("H*", $hexString));
+        foreach ($a as &$v) {
+            // convert each string byte to integer
+            $unp = unpack("H2b", $v);
+            //echo "\n".$unp['b'];
+            $hexString = $unp['b'];
+            $i = hexdec($hexString);
+            //echo "\n".$hexString." ".$i;
+            //replace string with integer in array:
+            $v = $i;
+        }
+        return $a;
+    }
 }
 
-	$c = new Curve25519();
-	print($c);
+$c = new Curve25519();
+print($c);
 //	echo "xxx".$c."\n";
 //	echo "yyy".Curve25519::$p;
--- php/tests/CurveTest.php
+++ php/tests/CurveTest.php
@@ -1,35 +1,62 @@
 <?php
-declare(strict_types=1);
+declare(strict_types = 1);
 
 use PHPUnit\Framework\TestCase;
+
+/**
+ * Get the type, resource name, or class name of a variable.
+ *
+ * Returns the type (name if an object or resource) of the PHP variable $var.
+ *
+ * @link   http://php.net/manual/en/function.gettype.php#104224
+ * @param  mixed $var The variable being type checked.
+ * @return string
+ */
+function get_var_type($var)
+{
+    if (is_object($var)) {
+        return get_class($var);
+    }
+    
+    if (is_resource($var)) {
+        return get_resource_type($var);
+    }
+    
+    return gettype($var);
+}
+
 // call like this: php phpunit.phar --bootstrap src/autoload.php --testdox tests
-
-
 final class CurveTest extends TestCase
 {
+
     public function testCanConvert(): void
     {
-    	  $crv = new Curve25519();
-        $this->assertInstanceOf(
-            Curve25519::class,
-            $crv
-        );
+        $crv = new Curve25519();
+        $this->assertInstanceOf(Curve25519::class, $crv);
         
-        echo $crv;
+        $scalar1 = "a546e36bf0527c9d3b16154b82465edd62144c0ac1fc5a18506a2244ba449ac4";
+        $ar1 = $crv->toByteArray($scalar1);
+        $this->assertEquals(32, count($ar1));
+        $this->assertEquals(0xa5, $ar1[0]);
+        $this->assertEquals(0xc4, $ar1[31]);
+        //$this->assertEquals(32, strlen($ar1));
+        // assertEquals(32, ar1.length);
+        // assertEquals((byte)0xa5, ar1[0]);
+        // assertEquals((byte)0xc4, ar1[31]);
     }
-
-//    public function testCannotBeCreatedFromInvalidEmailAddress(): void
-//    {
-//        $this->expectException(InvalidArgumentException::class);
-//
-//        Curve25519::fromString('invalid');
-//    }
-//
-//    public function testCanBeUsedAsString(): void
-//    {
-//        $this->assertEquals(
-//            'user@example.com',
-//            Curve25519::fromString('user@example.com')
-//        );
-//    }
+    
+    // public function testCannotBeCreatedFromInvalidEmailAddress(): void
+    // {
+    // $this->expectException(InvalidArgumentException::class);
+    //
+    // Curve25519::fromString('invalid');
+    // }
+    //
+    // public function testCanBeUsedAsString(): void
+    // {
+    // $this->assertEquals(
+    // 'user@example.com',
+    // Curve25519::fromString('user@example.com')
+    // );
+    // }
 }
