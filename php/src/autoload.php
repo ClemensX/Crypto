97cff979dddda3110d64e40436953d5d346573ff
Clemens Fehr <clemens.fehr@sap.com>
2018-06-14 12:48:37 +0200

    do not try to include phpunit classes

--- php/src/autoload.php
+++ php/src/autoload.php
@@ -1,6 +1,20 @@
 <?php
 spl_autoload_register(function ($class_name) {
-    include $class_name . '.php';
+    //include $class_name . '.php';
+	$query = "PHPUnit";
+	if (substr( $class_name, 0, strlen($query) ) === $query) {
+		return;
+	}
+	$query = "Symfony";
+	if (substr( $class_name, 0, strlen($query) ) === $query) {
+		return;
+	}
+	$query = "Composer";
+	if (substr( $class_name, 0, strlen($query) ) === $query) {
+		return;
+	}
+	//echo "autoload ".$class_name . '.php'."\n";
+	include $class_name . '.php';
 });
 
 //$obj1  = new Curve25519();
